b7f5f04f510b22068deffc0952f6201e75b883db
Walid <mabroukwalid0@gmail.com>
2025-11-05 15:22:46 +0100

    Etape 4

---
+++ calculatrice.js
@@ -0,0 +1,43 @@
+const display = document.getElementById('display');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+
+let number1 = '';
+let operator = '';
+let number2 = '';
+
+function updateDisplay() {
+    const left = number1 || '0';
+    const op = operator ? operator : '';
+    const right = number2 ? number2 : '';
+    display.innerHTML = left + op + right;
+}
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const digit = button.dataset.number;
+        if (operator === '') {
+            number1 = number1 === '' || number1 === '0' ? digit : number1 + digit;
+        } else {
+            number2 = number2 === '' || number2 === '0' ? digit : number2 + digit;
+        }
+        updateDisplay();
+    });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const op = button.dataset.operator;
+        if (number1 === '') {
+            return;
+        }
+        if (operator === '') {
+            operator = op;
+        } else if (number2 === '') {
+            operator = op;
+        } else {
+
+        }
+        updateDisplay();
+    });
+});
